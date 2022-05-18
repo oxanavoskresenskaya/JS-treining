ffad455a1866e9a36e3220d8fb40cafebed2f104
OxanaVoskresenskaya <oavoskresenskaya@mail.ru>
2022-05-18 17:35:31 +0300

    use Set for removing duplicates

--- index.js
+++ index.js
@@ -1,16 +1,8 @@
-var countBits = function(n) {
-    var x="";
-    let y=0;
-    for (let i = n; i > 0; i = Math.trunc(i / 2)) {
-       n=i%2;
-       x=n+x;
-    }
-    for (let k=0; k<x.length; k++) {
-        if (x[k]==1) {
-            y=y+(+x[k]);
-        };
-} 
-      return y;
-    }
-  let sw=countBits(1234);
-   console.log(sw);
+var uniqueInOrder=function(iterable){
+  
+  let arrUnique=new Set(iterable);
+  let res = [...arrUnique];
+  return res;
+}
+let sw=uniqueInOrder('AAAABBBCCDAABBB');
+console.log(sw);
